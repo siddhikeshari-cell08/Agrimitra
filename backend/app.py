1b96244bbb97870040977dd4a20fa5aaa3862a51
siddhikeshari-cell08 <kesharisiddhi00@gmail.com>
2026-08-21 00:17:03 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -138,9 +138,8 @@ def soil_health():
 # RUN SERVER
 # ============================================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
     app.run(
         host="0.0.0.0",
-        port=port,
+        port=int(os.environ.get("PORT", 5000)),
         debug=False
     )
