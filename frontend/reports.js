d9cc0429a01d4fe165703486f99d7b934c92781c
Siddhikeshari <siddhikeshari09@gmail.com>
2026-08-21 00:28:50 +0530

    Connect frontend to Render backend

--- frontend/reports.js
+++ frontend/reports.js
@@ -12,7 +12,7 @@ async function loadReport() {
         // ========================================
 
         const response = await fetch(
-            "http://127.0.0.1:5000/api/soil-health",
+        "https://agrimitra-1-ys6e.onrender.com/api/soil-health",
             {
                 method: "POST",
 
